a214e7558202c3d042d7da0388b7f16c3b43c0de
sonja <sonjeli93@gmail.com>
2022-01-07 13:27:20 +0200

    initial commit

---
+++ FarmDataApp/src/main/java/farmDataExercise/FarmDataApp/FarmDataAppApplication.java
@@ -0,0 +1,13 @@
+package farmDataExercise.FarmDataApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmDataAppApplication.class, args);
+	}
+
+}
---
+++ FarmDataApp/src/test/java/farmDataExercise/FarmDataApp/FarmDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package farmDataExercise.FarmDataApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
